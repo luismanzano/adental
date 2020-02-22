a6aedbb967f62241a2c550163874f687ef2ef96d
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2020-02-22 15:50:59 -0400

    anadido el sidebar como es

--- adental/src/app/app-routing.module.ts
+++ adental/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./component/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./component/home/home.module').then(m => m.HomeModule) }, { path: 'sidebar', loadChildren: () => import('./component/patient/sidebar/sidebar.module').then(m => m.SidebarModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
