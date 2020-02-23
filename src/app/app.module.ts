4e564d8902c0222bc12e8871c760205506b8a9fe
Luis Gonzalez <malared204@gmail.com>
2020-02-23 13:20:46 -0400

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarAdminComponent } from './admin/sidebar-admin/sidebar-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarAdminComponent
   ],
   imports: [
     BrowserModule,
