9200ae7e3e1ed383bc47adc28fa68a8365ca9d0b
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2020-02-23 14:08:04 -0400

    credo el componente perfil del paciente

--- adental/src/app/patient/patient.module.ts
+++ adental/src/app/patient/patient.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, ProfileComponent],
   imports: [
     CommonModule
   ]
