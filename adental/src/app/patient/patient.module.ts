92d1285e94868fa1699ce42315eacb7072017bae
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2020-02-23 16:59:42 -0400

    se creo el componente de citas

--- adental/src/app/patient/patient.module.ts
+++ adental/src/app/patient/patient.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent, ProfileComponent],
+  declarations: [SidebarComponent, ProfileComponent, AppointmentsComponent],
   imports: [
     CommonModule
   ],
