1cecb060085f2901bb800c0bbd9bc47eadd57d07
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-02-24 18:15:50 -0400

    djncnewjcne

--- adental/src/app/doctor/doctor.module.ts
+++ adental/src/app/doctor/doctor.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile_doctor/profile.component';
 import { SidebarDoctorComponent } from './sidebar-doctor/sidebar-doctor.component';
 import { NuevoPacienteComponent } from './nuevo-paciente/nuevo-paciente.component';
+import { EnviarMensajeComponent } from './enviar-mensaje/enviar-mensaje.component';
+import { MisPacientesComponent } from './mis-pacientes/mis-pacientes.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent, SidebarDoctorComponent, NuevoPacienteComponent],
+  declarations: [ProfileComponent, SidebarDoctorComponent, NuevoPacienteComponent, EnviarMensajeComponent, MisPacientesComponent],
   imports: [
     CommonModule
   ],
   exports:[
     ProfileComponent,
     SidebarDoctorComponent,
-    NuevoPacienteComponent
+    NuevoPacienteComponent,
+    EnviarMensajeComponent,
+    MisPacientesComponent
   ]
 })
 export class DoctorModule { }
