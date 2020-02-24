b543ab66b7e07b44fed9e43c4172c0d13865fb7f
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2020-02-24 17:45:47 -0400

    terminada bills

--- adental/src/app/patient/patient.module.ts
+++ adental/src/app/patient/patient.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AppointmentsComponent } from './appointments/appointments.component';
+import { BillsComponent } from './bills/bills.component';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent, ProfileComponent, AppointmentsComponent],
+  declarations: [SidebarComponent, ProfileComponent, AppointmentsComponent, BillsComponent],
   imports: [
     CommonModule
   ],
   exports: [
     ProfileComponent,
     SidebarComponent,
-    AppointmentsComponent
+    AppointmentsComponent,
+    BillsComponent
   ]
 })
 export class PatientModule { }
