2f8af2ae40d0536d3b18e59e1b669530684b6dc8
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-02-25 14:18:09 -0400

    cambio2

--- adental/src/app/doctor/profile_doctor/profile.component.ts
+++ adental/src/app/doctor/profile_doctor/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
+  selector: 'app-profile-doctor',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
