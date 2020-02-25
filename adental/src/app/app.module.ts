e6dbc7177f18f43c5209fd9115f236e786f15fef
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-02-25 17:26:31 -0400

    database implementado firebase

--- adental/src/app/app.module.ts
+++ adental/src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import{AngularFireModule} from '@angular/fire';
+import{AngularFireDatabaseModule} from '@angular/fire/database';
+import {environment} from '../environments/environment';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +22,9 @@ import { DoctorModule } from './doctor/doctor.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DoctorModule
+    DoctorModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- adental/src/environments/environment.ts
+++ adental/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBgsUDVwI3N73Ny_SEBNOInJIJyCrM0w5Q",
+    authDomain: "adental-69c48.firebaseapp.com",
+    databaseURL: "https://adental-69c48.firebaseio.com",
+    projectId: "adental-69c48",
+    storageBucket: "adental-69c48.appspot.com",
+    messagingSenderId: "504876035779",
+    appId: "1:504876035779:web:00c486d9c17111d4cdbc76",
+    measurementId: "G-906BQ8SN63"
+  }
 };
 
 /*
