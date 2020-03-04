c61c4edb41510018a2d9217bbba2028a5e37ac1d
Luis Manzano <luismanzano@Luiss-MacBook-Air.local>
2020-03-04 00:15:09 -0400

    ya se redirigen los usuarios

--- adental/src/app/component/home/home.component.ts
+++ adental/src/app/component/home/home.component.ts
@@ -4,6 +4,8 @@ import {FormsModule} from '@angular/forms';
 
 import { AuthService} from '../../core/services/auth.service';
 import any = jasmine.any;
+import { AngularFireAuth} from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-home',
@@ -15,30 +17,45 @@ export class HomeComponent implements OnInit {
   password: any;
   id: any;
   userFromDB: any;
+  testVar: string;
+  testVar = 'SdA3Q143pzSKYH3MVXbBRJIn7A43';
 
   constructor(
     private router: Router,
     private authService: AuthService,
-    private form: FormsModule
+    private form: FormsModule,
+    private af: AngularFireAuth
   ) { }
 
   ngOnInit() {
+
   }
+
+
 
   login() {
-    this.authService.login(this.user, this.password)
-      .then(() => {
-        alert('Usuario Logeado');
-        this.redirectUser();
-        //this.router.navigate(['/nuevo-usuario']);
-      })
-      .catch(() => alert('No se logeo'));
+    this.authService.login(this.user, this.password);
   }
 
-  redirectUser() {
+  setUser(user) {
+
+    // ESTE CODIGO DE ACA FUNCIONA NO TOCAR
+
+    // this.id = this.af.authState.subscribe( user => {
+    //   this.id = user.uid;
+    //   console.log(this.id);
+
+    this.af.authState.subscribe( user => {
+      this.id = user.uid;
+      console.log(this.id);
+
+    });
+  }
+
+  redirectUser(id: string) {
     alert('Esto esta funcionando');
-     this.authService.userData().subscribe( user => {
-       console.log(user.data() )
+    this.authService.userData(id).subscribe( user => {
+       console.log(user.data() );
        this.userFromDB = user.data();
      });
 
--- adental/src/app/core/services/auth.service.ts
+++ adental/src/app/core/services/auth.service.ts
@@ -3,14 +3,24 @@ import { AngularFireAuth} from '@angular/fire/auth';
 // import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFirestore} from '@angular/fire/firestore';
 import {} from '@angular/fire/database';
+import {Router} from "@angular/router";
+
+interface User {
+  name: string;
+  username: string;
+  id: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  mainUser: User;
+
   constructor(
-    private af: AngularFireAuth,
+    private router: Router,
+    public af: AngularFireAuth,
     private firestore: AngularFirestore
   ) { }
 
@@ -29,16 +39,50 @@ export class AuthService {
       });
   }
   login(email: string, password: string) {
-    return this.af.auth.signInWithEmailAndPassword(email, password);
+    // return this.af.auth.signInWithEmailAndPassword(email, password);
+    this.af.auth.signInWithEmailAndPassword(email, password)
+      .then((data) => {
+        console.log(data);
+        this.mainUser = {
+          name: 'hola',
+          username: 'loquesea@gmail.com',
+          id: data.user.uid
+        };
+        console.log(this.mainUser)
+        alert('Usuario Logeado');
+        this.redirectUser(this.mainUser.id);
+      })
+      .catch(() => alert('No se logeo'));
+
 
   }
-/*
-  theUser() {
-    return this.af.auth.currentUser();
+
+  redirectUser(id: string) {
+    alert('Esto esta funcionando');
+    this.userData(this.mainUser.id).subscribe( user => {
+      console.log(user.data() );
+      // tslint:disable-next-line:triple-equals
+      if (user.data().type == '2') {
+        this.router.navigate(['/nuevo-usuario']);
+      }
+      //this.userFromDB = user.data();
+    });
+
+    //console.log(this.userFromDB);
   }
-*/
-  userData() {
-    return this.firestore.collection('users').doc('SdA3Q143pzSKYH3MVXbBRJIn7A43').get();
+
+
+  getEstadoUsuario() {
+    return this.af.authState;
+  }
+
+  theUser() {
+    return this.af.auth.currentUser;
+  }
+
+
+  userData(data: string) {
+    return this.firestore.collection('users').doc(data).get();
 
   }
 
