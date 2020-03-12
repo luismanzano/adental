c0e8b61cec44e7fb40ebbe5d9451b39bc3e59e73
luismanzano <luisfmanzanoa@gmail.com>
2020-03-11 21:51:01 -0400

    Ya mandamos correosgit add .git add .

--- adental/functions/index.js
+++ adental/functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+
+require('dotenv').config()
+
+const {SENDER_EMAIL,SENDER_PASSWORD} = process.env;
+
+exports.sendEmailNotification = functions.firestore.document('submissions/{docId}').onCreate((snap, ctx) => {
+  const data = snap.data();
+
+  let authData = nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:465,
+    secure: true,
+    auth: {
+      user: 'luisfmanzanoa@gmail.com',
+      pass: 'accesstonewemail01*$'
+    }
+  });
+
+  authData.sendMail({
+    from:'adental@adental.com',
+    to: `${data.email}`,
+    subject: 'El usuario ha sido creado',
+    text: `${data.email}`,
+    html: `${data.email}`,
+  }).then(res => console.log('El mensaje fue enviado con exito')).catch(err => console.log(err));
+})
+
