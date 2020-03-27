4dd37761f93c2dfd78a5c14202dc84b28fdfa36e
luismanzano <luisfmanzanoa@gmail.com>
2020-03-26 21:20:30 -0400

    Se hizo paypal

--- adental/src/app/patient/payment/payment.component.ts
+++ adental/src/app/patient/payment/payment.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from 'src/app/core/services/auth.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AuthService} from 'src/app/core/services/auth.service';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+declare var paypal;
 
 @Component({
   selector: 'app-payment',
@@ -19,81 +21,112 @@ export class PaymentComponent implements OnInit {
   montoIngresado: number;
   total: number;
   private sub: any;
+  @ViewChild('paypal', { static: true}) paypalElement: ElementRef;
+
+
   constructor(
     private route: ActivatedRoute,
     private auth: AuthService,
     public firestore: AngularFirestore
-
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
 
+    paypal.Buttons({
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: 'Consulta De AdolfDentalCare',
+              amount: {
+                currency_code: 'USD',
+                value: this.montoPagar
+              }
+            }
+          ]
+        });
+      },
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        this.montoIngresado = this.montoPagar;
+        console.log(order)
+        this.pagar();
+      },
+      onError: err => {
+        console.log(err);
+      }
+    })
+      .render(this.paypalElement.nativeElement);
 
-    this.sub=this.route.params.subscribe(params=>{
-      this.idConsulta=params['id'];
+
+
+    this.sub = this.route.params.subscribe(params => {
+      this.idConsulta = params['id'];
     })
 
-    this.auth.consultaData(this.idConsulta).subscribe(consulta=>{
-      this.montoPagar=consulta.data().montoPago
-      this.fecha=consulta.data().createdAt
+    this.auth.consultaData(this.idConsulta).subscribe(consulta => {
+      this.montoPagar = consulta.data().montoPago
+      this.fecha = consulta.data().createdAt
     })
-    
 
-    this.transferencia=false;
-    this.zelle=false;
+
+    this.transferencia = false;
+    this.zelle = false;
     this.efectivo = false;
-    this.paypal=false;
+    this.paypal = false;
   }
 
-  irTransferencia(){
-    this.transferencia=true;
-    this.zelle=false;
+  irTransferencia() {
+    this.transferencia = true;
+    this.zelle = false;
     this.efectivo = false;
-    this.paypal=false;
+    this.paypal = false;
   }
 
-  irZelle(){
-    this.zelle=true;
-    this.transferencia=false;
+  irZelle() {
+    this.zelle = true;
+    this.transferencia = false;
     this.efectivo = false;
-    this.paypal=false;
+    this.paypal = false;
 
   }
 
-  irEfectivo(){
-    this.transferencia=false;
-    this.zelle=false;
+  irEfectivo() {
+    this.transferencia = false;
+    this.zelle = false;
     this.efectivo = true;
-    this.paypal=false;
+    this.paypal = false;
 
   }
 
-  irPaypal(){
-    this.transferencia=false;
-    this.zelle=false;
+  irPaypal() {
+    this.transferencia = false;
+    this.zelle = false;
     this.efectivo = false;
-    this.paypal=true;
+    this.paypal = true;
 
   }
 
-  
-pagar(){
-  const consultaRef=this.firestore.collection('consultas').doc(this.idConsulta);
-  if(this.montoIngresado==this.montoPagar){
-    consultaRef.update({
-      status: true,
-      montoPago:0
-    }).then(ref=>{
-      this.ngOnInit()
-    })
-  } else{
-    this.total=this.montoPagar-this.montoIngresado;
-    consultaRef.update({
-      montoPago:this.total
-    }).then(ref=>{
-      this.ngOnInit()
-    })
+
+  pagar() {
+    const consultaRef = this.firestore.collection('consultas').doc(this.idConsulta);
+    if (this.montoIngresado == this.montoPagar) {
+      consultaRef.update({
+        status: true,
+        montoPago: 0
+      }).then(ref => {
+        this.ngOnInit();
+        location.reload();
+      })
+    } else {
+      this.total = this.montoPagar - this.montoIngresado;
+      consultaRef.update({
+        montoPago: this.total
+      }).then(ref => {
+        this.ngOnInit()
+      })
+    }
   }
-}
 
 }
